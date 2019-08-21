becd114634245fb249795eba62512801b8228480
Alfredo Solano <asolano@weblab.t.u-tokyo.ac.jp>
2019-08-21 11:09:54 +0900

    Apply batches to extract.bash

--- doomrnn/extract.bash
+++ doomrnn/extract.bash
@@ -1,8 +1,23 @@
 export CUDA_VISIBLE_DEVICES=""
 
-for i in `seq 1 64`;
+# c1 cores in paper, c2 in current machine 
+# i.e., ZBOX1070 has only 4 cores, 64/4=16 batches
+c1=64
+c2=`cat /proc/cpuinfo | grep processor | wc -l`
+batches=$((c1 / c2))
+for b in `seq 1 $batches`;
 do
-  echo worker $i
-  python extract.py &
-  sleep 1.0
+  echo batch $b 
+  # Launch one process per core
+  for i in `seq 1 4`;
+    do
+      echo worker $i
+      python extract.py &
+      pids[${i}]=$!
+      sleep 1.0
+    done
+  # Wait for all processes to finish
+  for pid in ${pids[*]}; do
+    wait $pid
+  done
 done
